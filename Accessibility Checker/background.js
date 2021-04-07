e4e6f6c42edcd6410c970abede1afd2f09aaba5d
Erika Sundstrom <glasure7713@gmail.com>
2021-04-07 16:46:28 -0700

    added content.js and capability to retrieve the DOM

--- Accessibility Checker/background.js
+++ Accessibility Checker/background.js
@@ -1,6 +1,10 @@
-
+// A function to use as callback
+function doStuffWithDom(domContent) {
+    console.log('I received the following DOM content:\n' + domContent);
+}
 
 chrome.browserAction.onClicked.addListener(function(activeTab)
 {
+  chrome.tabs.sendMessage(activeTab.id, {text: "report_back"}, doStuffWithDom);
   chrome.windows.create({url: "popup.html", type: "popup", height: 250, width: 200, left: 940, top: 65});
 });
